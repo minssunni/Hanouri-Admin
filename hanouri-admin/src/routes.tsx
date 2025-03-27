37c0e27b886c59c9ba2fdfb5f85f6a18544d1424
Min Sun <minsunfiona.kim@fiserv.com>
2025-03-28 10:56:57 +1300

    add redirect for /* path

--- hanouri-admin/src/routes.tsx
+++ hanouri-admin/src/routes.tsx
@@ -1,12 +1,13 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Navigate, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Loginpage from './pages/LoginPage';
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/login" element={<Loginpage />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </>
     )
 )
